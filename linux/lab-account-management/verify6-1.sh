3c9145a103374426f1411e818e9eb26b458698cf
coolops-cn <baidjay@163.com>
2022-12-09 11:33:06 +0800

    update verify bash

--- linux/lab-account-management/verify6-1.sh
+++ linux/lab-account-management/verify6-1.sh
@@ -2,7 +2,7 @@
 
 # Check if the bob user has been deleted and the user directory is still saved.
 if [[ -d /home/bob ]]; then
-    sudo grep -w 'bob' /etc/passwd 
+    sudo grep -w 'bob' /etc/passwd || echo "pass"
 fi
     
     
