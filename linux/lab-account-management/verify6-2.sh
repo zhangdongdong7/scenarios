b84dd859363b4d51ba18ac0188b42ef07b2b0ec8
coolops-cn <baidjay@163.com>
2022-12-09 11:34:00 +0800

    update verify bash

--- linux/lab-account-management/verify6-2.sh
+++ linux/lab-account-management/verify6-2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Check if the jack user and his home diectory has been deleted.
-if [[ ! -d /home/adam ]]; then
+if [ ! -d /home/adam ]; then
     sudo grep -w 'jack' /etc/passwd || echo "pass"
 fi
