34d750fd641ce5ad51b95fd774249385de1ec7eb
huhuhang <huhuhang@users.noreply.github.com>
2023-04-17 06:20:47 +0000

    Formatting documents

--- linux/challenge-stream-editor-sed/verify1.sh
+++ linux/challenge-stream-editor-sed/verify1.sh
@@ -1,12 +1,12 @@
 #!/bin/zsh
 
-if [ -f result.txt ];then
-	result=`diff result.txt /tmp/verify1.txt | wc -l`
-	if [ $result -eq 0 ];then
-		exit 0
-	else
-		exit 1
-	fi
+if [ -f result.txt ]; then
+  result=$(diff result.txt /tmp/verify1.txt | wc -l)
+  if [ $result -eq 0 ]; then
+    exit 0
+  else
+    exit 1
+  fi
 else
-	exit 1
+  exit 1
 fi
--- linux/challenge-stream-editor-sed/verify4.sh
+++ linux/challenge-stream-editor-sed/verify4.sh
@@ -1,12 +1,12 @@
 #!/bin/zsh
 
-if [ -f result3.txt ];then
-	result=`diff result3.txt /tmp/verify4.txt | wc -l`
-	if [ $result -eq 0 ];then
-		exit 0
-	else
-		exit 1
-	fi
+if [ -f result3.txt ]; then
+  result=$(diff result3.txt /tmp/verify4.txt | wc -l)
+  if [ $result -eq 0 ]; then
+    exit 0
+  else
+    exit 1
+  fi
 else
-	exit 1
+  exit 1
 fi
