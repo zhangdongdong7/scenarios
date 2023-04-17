da32ad56bece512ca324516cec70d0ac92bdfc4d
huhuhang <huhuhang@users.noreply.github.com>
2023-04-17 05:54:51 +0000

    Formatting documents

--- linux/challenge-file-viewing/verify4.sh
+++ linux/challenge-file-viewing/verify4.sh
@@ -1,12 +1,12 @@
 #!/bin/zsh
 
-if [ -f step4.txt ];then
-	result=`diff step44.txt step4.txt | wc -l`
-	if [ $result -eq 0 ];then
-		exit 0
-	else
-		exit 1
-	fi
+if [ -f step4.txt ]; then
+  result=$(diff step44.txt step4.txt | wc -l)
+  if [ $result -eq 0 ]; then
+    exit 0
+  else
+    exit 1
+  fi
 else
-	exit 1
+  exit 1
 fi
