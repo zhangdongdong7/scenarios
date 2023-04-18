712378937bdcd80e4efa8bd162d4dab6ebcdd8aa
huhuhang <huhuhang@users.noreply.github.com>
2023-04-18 01:00:08 +0000

    Formatting documents

--- python/challenge-voting-system/assets/calculate_winner_test.py
+++ python/challenge-voting-system/assets/calculate_winner_test.py
@@ -5,6 +5,7 @@ sys.path.append("/home/labex/project")
 import unittest
 from calculate_winner import calculate_winner
 
+
 class TestCalculateWinner(unittest.TestCase):
     def test_calculate_winner(self):
         votes = ["john", "John", "Mary", "mary", "Peter", "PETER", "john"]
@@ -13,5 +14,6 @@ class TestCalculateWinner(unittest.TestCase):
         votes = ["john", "Mary", "peter", "Peter", "mary"]
         self.assertEqual(calculate_winner(votes), "Tie")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
