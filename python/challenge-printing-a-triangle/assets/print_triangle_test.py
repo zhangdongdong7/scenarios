bda25963ece7be992e839a66cafcf622a7ca90d3
huhuhang <huhuhang@users.noreply.github.com>
2023-04-18 01:52:20 +0000

    Formatting documents

--- python/challenge-printing-a-triangle/assets/print_triangle_test.py
+++ python/challenge-printing-a-triangle/assets/print_triangle_test.py
@@ -6,21 +6,23 @@ import unittest
 from io import StringIO
 from print_triangle import print_triangle
 
+
 class TestPrintTriangle(unittest.TestCase):
     def test_print_triangle(self):
-        expected_output = '*\n**\n***\n'
+        expected_output = "*\n**\n***\n"
         captured_output = StringIO()
         sys.stdout = captured_output
         print_triangle(3)
         sys.stdout = sys.__stdout__
         self.assertEqual(captured_output.getvalue(), expected_output)
 
-        expected_output = '*\n**\n***\n****\n*****\n'
+        expected_output = "*\n**\n***\n****\n*****\n"
         captured_output = StringIO()
         sys.stdout = captured_output
         print_triangle(5)
         sys.stdout = sys.__stdout__
         self.assertEqual(captured_output.getvalue(), expected_output)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
