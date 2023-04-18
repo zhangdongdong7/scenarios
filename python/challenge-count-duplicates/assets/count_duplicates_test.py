013fdbec68460642ad03f978e28cbc0febca5452
huhuhang <huhuhang@users.noreply.github.com>
2023-04-18 02:21:46 +0000

    Formatting documents

--- python/challenge-count-duplicates/assets/count_duplicates_test.py
+++ python/challenge-count-duplicates/assets/count_duplicates_test.py
@@ -5,13 +5,14 @@ sys.path.append("/home/labex/project")
 import unittest
 from count_duplicates import count_duplicates
 
+
 class TestCountDuplicates(unittest.TestCase):
     def test_count_duplicates(self):
-        self.assertEqual(count_duplicates('abcde'), 0)
-        self.assertEqual(count_duplicates('aabbcde'), 2)
-        self.assertEqual(count_duplicates('aabBBcde'), 2)
-        self.assertEqual(count_duplicates('indivisibility'), 1)
+        self.assertEqual(count_duplicates("abcde"), 0)
+        self.assertEqual(count_duplicates("aabbcde"), 2)
+        self.assertEqual(count_duplicates("aabBBcde"), 2)
+        self.assertEqual(count_duplicates("indivisibility"), 1)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
