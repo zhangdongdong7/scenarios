02763facc24dd0dad55280cf06b4ce1f1adacc5f
huhuhang <huhuhang@users.noreply.github.com>
2023-04-19 11:00:10 +0000

    Formatting documents

--- linux/challenge-group-of-linux/verify1.sh
+++ linux/challenge-group-of-linux/verify1.sh
@@ -2,8 +2,8 @@
 
 sudo cat /etc/group | grep "sales"
 RS=$?
-if [ $RS -eq 0 ];then
-	exit 0
+if [ $RS -eq 0 ]; then
+  exit 0
 else
-	exit 1
+  exit 1
 fi
--- linux/challenge-group-of-linux/verify3.sh
+++ linux/challenge-group-of-linux/verify3.sh
@@ -2,8 +2,8 @@
 
 sudo cat /etc/group | grep "sales"
 RS=$?
-if [ $RS -eq 0 ];then
-	exit 1
+if [ $RS -eq 0 ]; then
+  exit 1
 else
-	exit 0
+  exit 0
 fi
