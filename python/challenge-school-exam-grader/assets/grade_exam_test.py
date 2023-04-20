037c5a502c1a62ddd48f25927a20f167d637eba1
huhuhang <huhuhang@users.noreply.github.com>
2023-04-20 02:14:30 +0000

    Formatting documents

--- python/challenge-school-exam-grader/assets/grade_exam_test.py
+++ python/challenge-school-exam-grader/assets/grade_exam_test.py
@@ -6,26 +6,27 @@ import unittest
 import time
 from grade_exam import grade_exam
 
+
 class TestGradeExam(unittest.TestCase):
     def test_grade_exam(self):
         exam_scores1 = [85, 92, 78, 65, 90, 100, 70, 75, 82, 95]
         grade_dict1 = grade_exam(exam_scores1)
-        self.assertEqual(grade_dict1['date'], time.strftime("%m/%d/%Y"))
-        self.assertEqual(grade_dict1['A'], 4)
-        self.assertEqual(grade_dict1['B'], 2)
-        self.assertEqual(grade_dict1['C'], 3)
-        self.assertEqual(grade_dict1['D'], 1)
-        self.assertEqual(grade_dict1['F'], 0)
+        self.assertEqual(grade_dict1["date"], time.strftime("%m/%d/%Y"))
+        self.assertEqual(grade_dict1["A"], 4)
+        self.assertEqual(grade_dict1["B"], 2)
+        self.assertEqual(grade_dict1["C"], 3)
+        self.assertEqual(grade_dict1["D"], 1)
+        self.assertEqual(grade_dict1["F"], 0)
 
         exam_scores2 = [60, 70, 80, 90, 100]
         grade_dict2 = grade_exam(exam_scores2)
-        self.assertEqual(grade_dict2['date'], time.strftime("%m/%d/%Y"))
-        self.assertEqual(grade_dict2['A'], 2)
-        self.assertEqual(grade_dict2['B'], 1)
-        self.assertEqual(grade_dict2['C'], 1)
-        self.assertEqual(grade_dict2['D'], 1)
-        self.assertEqual(grade_dict2['F'], 0)
+        self.assertEqual(grade_dict2["date"], time.strftime("%m/%d/%Y"))
+        self.assertEqual(grade_dict2["A"], 2)
+        self.assertEqual(grade_dict2["B"], 1)
+        self.assertEqual(grade_dict2["C"], 1)
+        self.assertEqual(grade_dict2["D"], 1)
+        self.assertEqual(grade_dict2["F"], 0)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
-    
--- python/challenge-school-exam-grader/solutions/grade_exam.py
+++ python/challenge-school-exam-grader/solutions/grade_exam.py
@@ -1,25 +1,26 @@
 # This script imports the time module and defines a function that takes in a list of exam scores.
 import time
 
+
 def grade_exam(exam_scores):
     # Create a dictionary to store the grade counts.
-    grade_dict = {'A': 0, 'B': 0, 'C': 0, 'D': 0, 'F': 0}
+    grade_dict = {"A": 0, "B": 0, "C": 0, "D": 0, "F": 0}
 
     # Iterate over each exam score in the input list and update the grade counts accordingly.
     for score in exam_scores:
         if score >= 90:
-            grade_dict['A'] += 1
+            grade_dict["A"] += 1
         elif score >= 80:
-            grade_dict['B'] += 1
+            grade_dict["B"] += 1
         elif score >= 70:
-            grade_dict['C'] += 1
+            grade_dict["C"] += 1
         elif score >= 60:
-            grade_dict['D'] += 1
+            grade_dict["D"] += 1
         else:
-            grade_dict['F'] += 1
+            grade_dict["F"] += 1
 
     # Add the current date to the grade dictionary.
-    grade_dict['date'] = time.strftime("%m/%d/%Y")
+    grade_dict["date"] = time.strftime("%m/%d/%Y")
 
     # Return the grade dictionary.
     return grade_dict
