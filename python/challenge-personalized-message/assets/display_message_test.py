c3763066c17fe1df424b6d0c5c5af054cbcb39bf
huhuhang <huhuhang@users.noreply.github.com>
2023-04-21 03:36:25 +0000

    Formatting documents

--- python/challenge-personalized-message/assets/display_message_test.py
+++ python/challenge-personalized-message/assets/display_message_test.py
@@ -11,28 +11,28 @@ class TestDisplayMessage(unittest.TestCase):
     def test_display_message(self):
         test_cases = [
             {
-                'input': ('Alice', 25),
-                'expected_output': 'Hello, my name is Alice and I am 25 years old.\n'
+                "input": ("Alice", 25),
+                "expected_output": "Hello, my name is Alice and I am 25 years old.\n",
             },
             {
-                'input': ('Bob', 32),
-                'expected_output': 'Hello, my name is Bob and I am 32 years old.\n'
+                "input": ("Bob", 32),
+                "expected_output": "Hello, my name is Bob and I am 32 years old.\n",
             },
             {
-                'input': ('Charlie', 18),
-                'expected_output': 'Hello, my name is Charlie and I am 18 years old.\n'
-            }
+                "input": ("Charlie", 18),
+                "expected_output": "Hello, my name is Charlie and I am 18 years old.\n",
+            },
         ]
 
         for test_case in test_cases:
-            input_data = test_case['input']
-            expected_output = test_case['expected_output']
+            input_data = test_case["input"]
+            expected_output = test_case["expected_output"]
             captured_output = StringIO()
             sys.stdout = captured_output
             display_message(*input_data)
             sys.stdout = sys.__stdout__
             self.assertEqual(captured_output.getvalue(), expected_output)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
-    
--- python/challenge-personalized-message/solutions/display_message.py
+++ python/challenge-personalized-message/solutions/display_message.py
@@ -3,4 +3,4 @@
 def display_message(name, age):
     # The formatted string contains the 'name' and 'age' variables passed as arguments.
     # The 'f' in front of the string indicates it's an f-string, which allows variables to be directly embedded in the string.
-    print(f'Hello, my name is {name} and I am {age} years old.')
+    print(f"Hello, my name is {name} and I am {age} years old.")
