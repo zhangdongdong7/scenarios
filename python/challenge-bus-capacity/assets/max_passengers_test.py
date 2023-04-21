6757a5c39800219e63e6ecd53bc1c315c5ef2c00
huhuhang <huhuhang@users.noreply.github.com>
2023-04-21 03:33:37 +0000

    Formatting documents

--- python/challenge-bus-capacity/assets/max_passengers_test.py
+++ python/challenge-bus-capacity/assets/max_passengers_test.py
@@ -14,15 +14,15 @@ class TestMaxPassengers(unittest.TestCase):
     def test_max_passengers_all_full_buses(self):
         buses = [(50, 50), (40, 40), (60, 60)]
         self.assertEqual(max_passengers(buses), 0)
-        
+
     def test_max_passengers_all_empty_buses(self):
         buses = [(50, 0), (40, 0), (60, 0)]
         self.assertEqual(max_passengers(buses), 60)
-        
+
     def test_max_passengers_one_bus(self):
         buses = [(50, 30)]
         self.assertEqual(max_passengers(buses), 20)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
-    
