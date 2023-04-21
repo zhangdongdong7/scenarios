71916cba3137dfd87d52f0e1fb55a2a229519a79
huhuhang <huhuhang@users.noreply.github.com>
2023-04-21 03:27:24 +0000

    Formatting documents

--- kubernetes/lab-running-pod-with-daemonsets/verify2-2.sh
+++ kubernetes/lab-running-pod-with-daemonsets/verify2-2.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cat ~/.zsh_history | grep kubectl |grep apply
+cat ~/.zsh_history | grep kubectl | grep apply
 minikube kubectl -- get daemonset | grep "myapp-daemonset"
--- kubernetes/lab-running-pod-with-daemonsets/verify3-2.sh
+++ kubernetes/lab-running-pod-with-daemonsets/verify3-2.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cat ~/.zsh_history | grep kubectl |grep get |grep pod | grep app=myapp 
+cat ~/.zsh_history | grep kubectl | grep get | grep pod | grep app=myapp
