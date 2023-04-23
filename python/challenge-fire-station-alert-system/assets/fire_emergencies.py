bb432bd6f944f0de7d56791992fa3f78a11098cf
Likes learning-Liu <125553985+Liugaga-vvv@users.noreply.github.com>
2023-04-23 10:34:43 +0800

    Challenge fire station alert system (#564)
    * add challenge-the-powerful-numpy-einsum-function
    
    * add challenge-the-powerful-numpy-einsum-function
    
    * Delete numpy/challenge-the-powerful-numpy-einsum-function directory
    
    * add challenge-fire-station-alert-system
    
    * Update index.json
    
    * add challenge-fire-station-alert-system
    
    * Update step1.md

---
+++ python/challenge-fire-station-alert-system/assets/fire_emergencies.py
@@ -0,0 +1,26 @@
+class Emergency:
+    def __init__(self, priority, location):
+        # TODO: Write your code here
+        pass
+
+    def __del__(self):
+        # TODO: Write your code here
+        pass
+
+
+class FireStation:
+    def __init__(self):
+        # TODO: Write your code here
+        pass
+
+        def add_emergency(self, emergency):
+            # TODO: Write your code here
+            pass
+
+        def remove_emergency(self, emergency):
+            # TODO: Write your code here
+            pass
+
+        def prioritize_emergencies(self):
+            # TODO: Write your code here
+            pass
