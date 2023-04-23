7047c196266f43f6936394a1df4c80e567db5aaf
huhuhang <huhuhang@users.noreply.github.com>
2023-04-23 02:31:13 +0000

    Formatting documents

--- python/challenge-warehouse-inventory/assets/update_inventory_test.py
+++ python/challenge-warehouse-inventory/assets/update_inventory_test.py
@@ -5,17 +5,17 @@ sys.path.append("/home/labex/project")
 import unittest
 from update_inventory import update_inventory
 
+
 class TestUpdateInventory(unittest.TestCase):
-    
     def test_update_inventory(self):
         inventory = {"apple": 10, "banana": 20, "orange": 30}
         orders = [("apple", 5, "A1"), ("banana", 10, "B2"), ("pear", 5, "C3")]
-        expected_output = {'apple': 5, 'banana': 10, 'orange': 30}
+        expected_output = {"apple": 5, "banana": 10, "orange": 30}
         self.assertEqual(update_inventory(inventory, orders), expected_output)
-        
+
         inventory = {"apple": 10, "banana": 20, "orange": 30}
         orders = [("apple", 5, "A1"), ("banana", 30, "B2"), ("pear", 5, "C3")]
-        expected_output = {'apple': 5, 'banana': 20, 'orange': 30}
+        expected_output = {"apple": 5, "banana": 20, "orange": 30}
         self.assertEqual(update_inventory(inventory, orders), expected_output)
 
         inventory = {"apple": 10, "banana": 20, "orange": 30}
@@ -23,6 +23,6 @@ class TestUpdateInventory(unittest.TestCase):
         expected_output = {"apple": 10, "banana": 10, "orange": 30}
         self.assertEqual(update_inventory(inventory, orders), expected_output)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
-    
