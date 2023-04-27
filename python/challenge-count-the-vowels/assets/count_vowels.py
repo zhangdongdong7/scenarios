fc4b0b885d310d5a0cdbc97636ff97f07ed785fa
Weng hanwen <105929020+AsukaLove2@users.noreply.github.com>
2023-04-27 08:46:05 +0800

    Add challenge-count-the-vowels (#570)
    * Add files via upload
    
    * Update index.json
    
    * Add files via upload
    
    * Add files via upload
    
    * Update count_the_vowels_test.py
    
    * Add files via upload

---
+++ python/challenge-count-the-vowels/assets/count_vowels.py
@@ -0,0 +1,11 @@
+def count_vowels(string: str) -> int:
+    """Count the number of vowels in the string and return it as an integer
+
+    Args:
+        string (str): The string to be read
+
+    Returns:
+        int: The number of vowels counted
+    """
+    # Write your solution here.
+    pass
