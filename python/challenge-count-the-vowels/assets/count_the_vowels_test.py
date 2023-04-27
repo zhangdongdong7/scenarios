4b295ff74dbfa266c33b368f2d77d7777c46a065
huhuhang <huhuhang@users.noreply.github.com>
2023-04-27 00:46:31 +0000

    Formatting documents

--- python/challenge-count-the-vowels/assets/count_the_vowels_test.py
+++ python/challenge-count-the-vowels/assets/count_the_vowels_test.py
@@ -5,8 +5,8 @@ sys.path.append("/home/labex/project")
 
 from count_vowels import count_vowels
 
+
 class TestCountVowels(unittest.TestCase):
-    
     def test_count_vowels(self):
         self.assertEqual(count_vowels("hello"), 2)
         self.assertEqual(count_vowels("world"), 1)
@@ -14,5 +14,6 @@ class TestCountVowels(unittest.TestCase):
         self.assertEqual(count_vowels("aio"), 3)
         self.assertEqual(count_vowels("vwxyz"), 0)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
