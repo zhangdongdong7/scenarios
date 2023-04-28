9807cc5d10f260370a7e781202776f6a291aa39c
huhuhang <huhuhang@users.noreply.github.com>
2023-04-28 07:05:48 +0000

    Formatting documents

--- python/challenge-get-local-time/assets/get_local_time.py
+++ python/challenge-get-local-time/assets/get_local_time.py
@@ -2,8 +2,9 @@ import requests
 import datetime
 import pytz
 
+
 def get_local_time(city: str):
-    #TODO: complete the function
+    # TODO: complete the function
     """
     This function takes a city name as a parameter and returns the local time of that city.
     :param city: The name of the city.
